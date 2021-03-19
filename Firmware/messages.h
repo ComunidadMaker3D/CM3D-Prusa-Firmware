300d767370e4be8fce53d33f7cc3d2142bb1758f
jfestrada <jfestrada@cloudhd.eu>
2021-03-19 17:26:57 +0100

    Added a new G-Code (M880) in order to check the Steel Sheet which is installed and remember to the user throught a message on the LCD screen

--- Firmware/messages.h
+++ Firmware/messages.h
@@ -207,6 +207,7 @@ extern const char MSG_ADVANCE_K[];
 extern const char MSG_POWERPANIC_DETECTED[];
 extern const char MSG_LCD_STATUS_CHANGED[];
 extern const char MSG_UNKNOWN_CODE[];
+extern const char MSG_CHECK_STEEL_SHEET[];
 
 #if defined(__cplusplus)
 }
