1bd5e3d4f0907f8a9198b41f3003ab7157b4979f
Juan Francisco Estrada <juanfra@cloudhd.eu>
2021-12-23 23:26:53 +0100

    Languaje corrections and steel sheet reminder

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -7075,6 +7075,7 @@ Sigma_Exit:
 	- U - Firmware version provided by G-code to be compared to current one.  
 	*/
 	case 115: // M115
+      steel_sheet_check();
       if (code_seen('V')) {
           // Report the Prusa version number.
           SERIAL_PROTOCOLLNRPGM(FW_VERSION_STR_P());
@@ -8479,7 +8480,6 @@ Sigma_Exit:
           switch((ClPrintChecking)nCommand)
                {
                case ClPrintChecking::_Nozzle:     // ~ .1
-                    steel_sheet_check();
                     uint16_t nDiameter;
                     if(code_seen('P'))
                          {
