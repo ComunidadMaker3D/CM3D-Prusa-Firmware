f343609c1c4b57f9e308197f2346f5b9268aa3ba
Juan Francisco Estrada <juanfra@cloudhd.eu>
2022-01-21 10:04:35 +0100

    Fix the steel sheet check on usb print

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -7075,7 +7075,10 @@ Sigma_Exit:
 	- U - Firmware version provided by G-code to be compared to current one.  
 	*/
 	case 115: // M115
-      steel_sheet_check();
+      if (IS_SD_PRINTING)
+      {
+        steel_sheet_check();
+      }
       if (code_seen('V')) {
           // Report the Prusa version number.
           SERIAL_PROTOCOLLNRPGM(FW_VERSION_STR_P());
